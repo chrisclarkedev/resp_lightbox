e3c3d4c4847d827ad42d1b4f7e5abbba23e1b333
chrisclarkedev <cclarke8904@gmail.com>
2021-08-30 19:41:47 -0400

    created gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export default function Gallery() {
   return (
-    <div id="tester">
-      <img src={require('./images/image1.jpg')} />
+    <div class="gallery">
+      <li class="buttons" data-filter="all">
+        all
+      </li>
+      <li class="buttons" data-filter="ferris wheels">
+        ferris wheels
+      </li>
+      <li class="buttons" data-filter="rollercoasters">
+        rollercoasters
+      </li>
+      <li class="buttons" data-filter="carousels">
+        carousels
+      </li>
+      <li class="buttons" data-filter="water slides">
+        water slides
+      </li>
     </div>
   );
 }

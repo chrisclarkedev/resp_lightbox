981be8500dbfe1a3238444630261296a4c29c04e
chrisclarkedev <cclarke8904@gmail.com>
2021-08-31 18:26:49 -0400

    passing images is broken

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
-import Image1 from './images/car1.jpg';
-import Image2 from './images/fw1.jpg';
-import Image3 from './images/roll1.jpg';
-import Image4 from './images/ws1.jpg';
+import Img1 from './images/car1.jpg';
+import Img2 from './images/car2.jpg';
+import Img3 from './images/car3.jpg';
 
-export default function Gallery() {
+const Gallery = () => {
   let data = [
     {
       id: 1,
-      imgSrc: 'Image1',
+      imgSrc: 'Img1',
     },
     {
       id: 2,
-      imgSrc: 'Image2',
+      imgSrc: 'Img2',
     },
     {
       id: 3,
-      imgSrc: 'Image3',
-    },
-    {
-      id: 4,
-      imgSrc: 'Image4',
+      imgSrc: 'Img3',
     },
   ];
+
   return (
-    <div class="gallery">
-      <h1>Gallery</h1>
-      <>
-        <div>{console.log(data)}</div>
-      </>
-    </div>
+    <>
+      <div className="gallery">
+        {data.map((id, index) => {
+          return (
+            <div className="pics" key={index}>
+              <img src={id.imgSrc} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
+
+export default Gallery;
